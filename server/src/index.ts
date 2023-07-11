c55665d0c8bb11b794fa84518a1a1165a127f215
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-07-11 21:08:14 +0600

    openai setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response } from 'express';
 const dotenv = require('dotenv');
+const { Configuration, OpenAIApi } = require("openai");
+const cors = require("cors")
 
 dotenv.config();
 
+
 const app: Express = express();
 const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript + OpenAI Server');
